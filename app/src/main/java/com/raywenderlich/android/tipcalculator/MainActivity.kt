de8091f432c3cbe79f280791856cd3ab98114d05
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2018-11-18 15:25:20 +0530

    Add viewmodel and livedata dependencies

--- app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.raywenderlich.android.tipcalculator
 
+import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+  private lateinit var viewModel: MainActivityViewModel
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    // Instantiate ViewModel
+    viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
   }
 }
---
+++ app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivityViewModel.kt
@@ -0,0 +1,10 @@
+package com.raywenderlich.android.tipcalculator
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * Created by kalyandechiraju on 18/11/18.
+ * Playo
+ */
+class MainActivityViewModel: ViewModel() {
+}
