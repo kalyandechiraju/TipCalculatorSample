5738a6ceb6e6d72a41949469da0b114610a4ec35
Kalyan Dechiraju <kalyandechiraju@outlook.com>
2018-11-18 15:50:21 +0530

    Add tip computation logic

--- app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.raywenderlich.android.tipcalculator
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
   private lateinit var viewModel: MainActivityViewModel
+
+  private lateinit var resultTextView: TextView
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -14,5 +20,36 @@ class MainActivity : AppCompatActivity() {
 
     // Instantiate ViewModel
     viewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+
+    setupView()
+  }
+
+  private fun setupView() {
+    val numberOfPeople = findViewById<EditText>(R.id.activity_main_people_count_value)
+    val totalBill = findViewById<EditText>(R.id.activity_main_total_bill_value)
+    val tipPercentage = findViewById<EditText>(R.id.activity_main_tip_percent_value)
+    resultTextView = findViewById(R.id.activity_main_result)
+    val calculateButton = findViewById<Button>(R.id.activity_main_calculate_button)
+    calculateButton.setOnClickListener {
+      when {
+        numberOfPeople.text?.toString()?.isBlank() == true -> numberOfPeople.error = "Enter number of people"
+        totalBill.text?.toString()?.isBlank() == true -> totalBill.error = "Enter the bill amount"
+        tipPercentage.text?.toString()?.isBlank() == true -> tipPercentage.error = "Enter tip percentage"
+        else -> viewModel.computeTip(
+            numberOfPeople = numberOfPeople.text.toString().toInt(),
+            tipPercentage = tipPercentage.text.toString().toDouble(),
+            totalBill = totalBill.text.toString().toDouble()
+        )
+      }
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    viewModel.result.observe(this, Observer { result ->
+      result?.let {
+        resultTextView.text = it
+      }
+    })
   }
 }
--- app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivityViewModel.kt
+++ app/src/main/java/com/raywenderlich/android/tipcalculator/MainActivityViewModel.kt
@@ -1,10 +1,23 @@
 package com.raywenderlich.android.tipcalculator
 
+import android.arch.lifecycle.LiveData
+import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.ViewModel
 
 /**
  * Created by kalyandechiraju on 18/11/18.
  * Playo
  */
-class MainActivityViewModel: ViewModel() {
+class MainActivityViewModel : ViewModel() {
+  private val _result: MutableLiveData<String> = MutableLiveData()
+  val result: LiveData<String>
+    get() = _result
+
+  fun computeTip(numberOfPeople: Int, tipPercentage: Double, totalBill: Double) {
+    val tipAmount = (totalBill * tipPercentage) / 100
+    val totalAmountToPay = tipAmount + totalBill
+    val billPerPerson = totalAmountToPay / numberOfPeople
+    _result.postValue(String.format("$%.2f", billPerPerson))
+  }
+
 }
